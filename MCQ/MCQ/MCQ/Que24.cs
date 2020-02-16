21dfd7487c7915ba938bc08c582f41db67c27b31
Deepak <Deepakgullipalli@gmail.com>
2020-02-16 23:31:24 +0530

    Feature/MCQ Ignore bin and obj folder while checking in

---
+++ MCQ/MCQ/MCQ/Que24.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCQ.MCQ
+{
+    class Que24
+    {
+        static Que24()
+        {
+            Console.WriteLine("Hi I'm Static");
+        }
+        public Que24()
+        {
+            Console.WriteLine("Hi I'm Public");
+        }
+    }
+}
